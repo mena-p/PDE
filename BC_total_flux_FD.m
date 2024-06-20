c075815cb05960052e3b9b7591c55520ab5ad19e
mena-p <119264054+mena-p@users.noreply.github.com>
2024-06-20 19:44:07 +0200

    deleted comments

--- BC_total_flux_FD.m
+++ BC_total_flux_FD.m
@@ -1,8 +1,5 @@
 function flux = BC_total_flux_FD(height,position)
-%BC_NORMAL_FD Summary of this function goes here
-%   Detailed explanation goes here
 
 flux = 0.13/(5*sqrt(2*pi)) * exp(-0.5*(position./5).^2);
 
 end
-
